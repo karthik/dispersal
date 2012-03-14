97fefdccacaec52c0fa49d137cf811b63b7e0ce4
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 23:34:54 -0700

    getting my act together.

--- load.r
+++ load.r
@@ -1,13 +1,11 @@
-# This is a trial run of the Beetle disepersal data in R.
-# Feb 10, 2011
-# `````````````````````````````````````````````````````````````````````
+# New effort to wrap up my dispersal experiment.
 rm(list=ls())
-library(ggplot2)
-setwd('~/Documents/Work/Active Projects/Dispersal/analysis_2011/')
-load('base_dispersal_data.rdata')
-rm(con,year1,year2) #don't need the MySQL connection anymore
-
+setwd('~/Github/postdoc/dispersal_analysis')
+library(data.table)
+library(plyr)
+library(RMySQL)
+# I seem to have skipped loading the RAW data from MySQL here.
+load('data/base_dispersal_data.rdata')
+rm(con,year1,year2) #don't need the MySQL connection 
 names(dispersal)= c("loc","dist","depth","wax1","wax2","wax3","year","moisture","true_yr","plot","week","trt","d1","d2","who")
-
-save.image(file="dispersal_cleaned.rdata")
-# use this ^ file going forward #
+save(dispersal, file = "data/cleaned_dispersal_data.rdata")
