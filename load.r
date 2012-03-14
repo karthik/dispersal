634150f5bdb9464e86a6174debd40263501e31ae
Karthik Ram <karthik.ram@gmail.com>
2012-03-14 00:09:12 -0700

    Formatted load and moved cleaning components to clean.r

--- load.r
+++ load.r
@@ -1,25 +1,15 @@
 # New effort to wrap up my dispersal experiment.
+# Analysis of the dispersal data from the lower-draw plots. 
+# Data spans 2 years (2007, 2008).
 rm(list=ls())
 setwd('~/Github/postdoc/dispersal_analysis')
-library(data.table)
+
 library(plyr)
 library(RMySQL)
-# I seem to have skipped loading the RAW data from MySQL here.
-load('data/base_dispersal_data.rdata')
-rm(con,year1,year2) #don't need the MySQL connection 
-names(dispersal)= c("loc","dist","depth","wax1","wax2","wax3","year","moisture","true_yr","plot","week","trt","d1","d2","who")
-save(dispersal, file = "data/cleaned_dispersal_data.rdata")
 
-# Older stuff I need to integrate
-
-library(RMySQL)
-library(ggplot2)
-
-con<-dbConnect(MySQL(),user="root",password="wfish82",dbname="dispersal",host="localhost")
+# Grabbing data from the database
+con<-dbConnect(MySQL(),user="root",password=getOption("dbpass"),dbname="nematode",host="localhost")
 year1=dbGetQuery(con,"select * from year1")
-year1$TOTGOLDS= year1$W1G+year1$W2G+year1$W3G
-
-plot1= ggplot() + geom_bar(data=year1, aes(x=Distance, ymax=TOTGOLDS), colour="steelblue", fill="steelblue", size=1, alpha=0.5, shape=1)
-plot2 = plot1 + facet_grid(TRT ~ PLOT)
-plot2
-write(year1, file="year1.csv", sep=",")
+year2=dbGetQuery(con,"select * from year2")
+# Sinking raw, unmanipulated data to disk.
+save(year1, year2, file = "data/raw_dispersal.rdata")
