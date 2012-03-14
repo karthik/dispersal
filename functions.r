04a18482c4d40b225fe09fb855824914e9a748c7
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 23:45:33 -0700

    Moving functions from clean.r to do.r

--- functions.r
+++ functions.r
@@ -1,20 +1,9 @@
-#````````````````````````````````````````````````````
-# This script contains functions for analyzing the dispersal dataset
-# Author: Karthik Ram <kram@ucsc.edu>
+# Functions for analzing MY dispersal data.
 
-#````````````````````````````````````````````````````
-
-rm(list=ls())
-len=length
- 
-# 1.  >>>> Settings for the dispersal plots. >>>>
-
-no_small_grid = opts(panel.grid.minor=theme_line(colour=NA))
-no_large_grid = opts(panel.grid.major=theme_line(colour=NA))
-large_x = opts(axis.text.x=theme_text(size=10,vjust=1.2,colour="black"))
-large_y = opts(axis.text.y=theme_text(size=10,hjust=1.2,colour="black"))
-no_bg = opts(panel.background=theme_rect(colour=NA))
-draw_axis = opts(axis.line=theme_segment(colour="black",linetype="solid",size=0.8))
-x_a_ticks = opts(axis.title.x=theme_text(colour="black",size=14),vjust=-1)
-y_a_ticks = opts(axis.title.y=theme_text(colour="black",size=14,angle=90))
-my_scales = list(no_small_grid,no_large_grid,no_bg,draw_axis,x_a_ticks,y_a_ticks)
+agg <- function (data)
+{
+dat=ddply(data, .(dist), transform, count=sum(total))
+dat=dat[,c(2,7,8,9,11)]
+dat2=unique(dat)
+return (dat2)
+}
