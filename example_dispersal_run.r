46113c4ef9995686e35002012ea4b1d89456939f
Karthik Ram <karthik.ram@gmail.com>
2012-03-13 23:37:22 -0700

    deleted fairly useless file.

--- example_dispersal_run.r
+++
@@ -1,7 +0,0 @@
-# This is a trial run of the Beetle disepersal data in R.
-# Feb 10, 2011
-# `````````````````````````````````````````````````````````````````````
-rm(list=ls())
-library(ggplot2)
-setwd('~/Documents/Work/Active Projects/Dispersal/analysis/')
-load('dispersal1.rdata')
