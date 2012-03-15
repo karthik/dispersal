145e45829cf1e724b38223193f6ecd83acf9b934
Karthik Ram <karthik.ram@gmail.com>
2012-03-15 12:51:17 -0700

    updated my first summary of the data.

--- updated_do.r
+++ updated_do.r
@@ -6,15 +6,14 @@ library(stringr)
 library(reshape2)
 load("data/cleaned_disp_data.rdata")
 
-# remove true year temporarily
-disp_data <- disp_data[, -which(names(disp_data)=="true_year")]
 
 # Data summaries
 s1 <- melt(disp_data, id.vars=1:7)
-s2 <- dcast(s1, trt + period + week ~ variable, mean)
-ggplot(subset(s2,period=="Y1"), aes(week,total_hits, colour=trt)) + geom_point(aes(shape=s2$period),size=3.2) + ylab("Mean number of hits")
-s1a <- melt(s1, id.vars=1:2)
+
+s2 <- dcast(subset(s1, period == "Y1"), trt + distance ~ week, mean)
+s3 <- melt(s2, id.vars=1:2)
+ggplot(s3, aes(distance, value, colour=trt)) + geom_point(size=3, alpha=0.8) + facet_wrap(~variable, nrow=2, ncol=3, scales = "free")
 # Analyses
 
 # Plots
-
+ 
