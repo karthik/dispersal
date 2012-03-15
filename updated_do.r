d691912f66e4b01bb2b8c73d6edc220a4f910aa2
Karthik Ram <karthik.ram@gmail.com>
2012-03-14 20:01:44 -0700

    Wrote first summary function for the do file.

--- updated_do.r
+++ updated_do.r
@@ -1,11 +1,19 @@
 rm(list=ls())
+setwd('~/Github/postdoc/dispersal_analysis')
 library(plyr)
 library(ggplot2)
 library(stringr)
+library(reshape2)
 load("data/cleaned_disp_data.rdata")
 
-# Data summaries
+# remove true year temporarily
+disp_data <- disp_data[, -which(names(disp_data)=="true_year")]
 
+# Data summaries
+s1 <- melt(disp_data, id.vars=1:7)
+s2 <- dcast(s1, trt + period + week ~ variable, mean)
+ggplot(subset(s2,period=="Y1"), aes(week,total_hits, colour=trt)) + geom_point(aes(shape=s2$period),size=3.2) + ylab("Mean number of hits")
+s1a <- melt(s1, id.vars=1:2)
 # Analyses
 
 # Plots
